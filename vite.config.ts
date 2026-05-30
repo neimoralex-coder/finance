9af69e792fd8f5fc25f00d3774bd788526ae3d0b
Пятница <patnica@MacBook-Pro-Patnica.local>
2026-05-30 22:32:59 +0500

    Исправлен base для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(async () => {
-  const plugins = [react(), tailwindcss()];
+  const plugins = [react(), tailwindcss()]
+
   try {
     // @ts-ignore
-    const m = await import('./.vite-source-tags.js');
-    plugins.push(m.sourceTags());
+    const m = await import('./.vite-source-tags.js')
+    plugins.push(m.sourceTags())
   } catch {}
-  return { plugins };
+
+  return {
+    plugins,
+    base: '/finance/',
+  }
 })
